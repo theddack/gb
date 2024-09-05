81748313cf44aa4f2c03c2a7fd10ecff07d1f720
KimTaeHwan <85102749+metacare-dev@users.noreply.github.com>
2024-09-05 14:34:36 +0900

    쇼핑 페이지 삭제

---
+++ gb/adm/company_browse.php
@@ -0,0 +1,43 @@
+<?php
+$sub_menu = "300510";
+require_once './_common.php';
+
+auth_check_menu($auth, $sub_menu, 'r');
+
+$g5['title'] = '둘러보기';
+require_once './admin.head.php';
+
+?>
+<div class="local_ov01 local_ov">
+    <span class="btn_ov01">
+        <span class="ov_txt">전체 </span>
+        <span class="ov_num"> 
+            <?php echo $total_count; ?>건
+        </span>
+    </span>
+</div>
+
+<div class="btn_fixed_top ">
+    <a href="./company_browse_insert.php" class="btn btn_01">등록</a>
+</div>
+
+<div class="tbl_head01 tbl_wrap">
+    <table>
+        <tr>
+            <th scope="col">번호</th>
+            <th scope="col">지점</th>
+            <th scope="col">이미지</th>
+            <th scope="col">등록자</th>
+            <th scope="col">수정자</th>
+            <th scope="col">수정일</th>
+            <th scope="col">등록일</th>
+            <th scope="col">관리</th>
+        </tr>
+    </table>
+
+
+</div>
+
+
+<?php
+require_once './admin.tail.php';
--- gb/theme/basic/head.php
+++ gb/theme/basic/head.php
@@ -42,52 +42,7 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
         <div id="logo">
             <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_IMG_URL ?>/logo.png" alt="<?php echo $config['cf_title']; ?>"></a>
         </div>
-    
-        <div class="hd_sch_wr">
-            <fieldset id="hd_sch">
-                <legend>사이트 내 전체검색</legend>
-                <form name="fsearchbox" method="get" action="<?php echo G5_BBS_URL ?>/search.php" onsubmit="return fsearchbox_submit(this);">
-                <input type="hidden" name="sfl" value="wr_subject||wr_content">
-                <input type="hidden" name="sop" value="and">
-                <label for="sch_stx" class="sound_only">검색어 필수</label>
-                <input type="text" name="stx" id="sch_stx" maxlength="20" placeholder="검색어를 입력해주세요">
-                <button type="submit" id="sch_submit" value="검색"><i class="fa fa-search" aria-hidden="true"></i><span class="sound_only">검색</span></button>
-                </form>
 
-                <script>
-                function fsearchbox_submit(f)
-                {
-                    var stx = f.stx.value.trim();
-                    if (stx.length < 2) {
-                        alert("검색어는 두글자 이상 입력하십시오.");
-                        f.stx.select();
-                        f.stx.focus();
-                        return false;
-                    }
-
-                    // 검색에 많은 부하가 걸리는 경우 이 주석을 제거하세요.
-                    var cnt = 0;
-                    for (var i = 0; i < stx.length; i++) {
-                        if (stx.charAt(i) == ' ')
-                            cnt++;
-                    }
-
-                    if (cnt > 1) {
-                        alert("빠른 검색을 위하여 검색어에 공백은 한개만 입력할 수 있습니다.");
-                        f.stx.select();
-                        f.stx.focus();
-                        return false;
-                    }
-                    f.stx.value = stx;
-
-                    return true;
-                }
-                </script>
-
-            </fieldset>
-                
-            <?php echo popular('theme/basic'); // 인기검색어, 테마의 스킨을 사용하려면 스킨을 theme/basic 과 같이 지정  ?>
-        </div>
         <ul class="hd_login">        
             <?php if ($is_member) {  ?>
             <li><a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=<?php echo G5_BBS_URL ?>/register_form.php">정보수정</a></li>
